45e963f12d3dec39133cd3c0ac57ceba37cd62cb
MeesBreedveld <mees.breedveld@gmail.com>
2025-07-20 22:29:00 +0200

    Clean up project structure - removed unnecessary deployment guides and scripts

--- MyWebsite/obj/Release/net9.0/MyWebsite.AssemblyInfo.cs
+++ MyWebsite/obj/Release/net9.0/MyWebsite.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebsite")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30207c043eca742f7c1e3b32c0a38f13437f3533")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebsite")]
